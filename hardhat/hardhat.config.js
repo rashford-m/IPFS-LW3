5bdf2e8a2a2f3c318546b4a63e4a2e87f0b9dd69
Rashford <mikearowo@gmail.com>
2023-09-24 20:13:21 +0100

    new

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -5,7 +5,7 @@ const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.8",
   networks: {
     mumbai: {
       url: QUICKNODE_HTTP_URL,
